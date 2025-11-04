42260ce66a0ee351c66620bcb9214c88bb04b4e6
HamzaSairafi <154326250+HamzaSairafi@users.noreply.github.com>
2025-11-04 20:45:14 +0300

    Inital commit

---
+++ test.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded' , () => {// saying wait till all of the HTML page is loaded befor starting the code
+    const grid = document.querySelector('#grid')
+})
